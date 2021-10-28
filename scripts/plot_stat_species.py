5f694309ca76cc865f071256a5f5977c04f2c7a1
sy520 <36355222+zxgsy520@users.noreply.github.com>
2021-10-28 11:27:36 +0800

    Add files via upload

--- scripts/plot_stat_species.py
+++ scripts/plot_stat_species.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#coding:utf-8
 
+import os
+import re
 import sys
 import logging
 import argparse
@@ -8,114 +10,130 @@ import matplotlib
 matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
-from matplotlib import patches as mpatches
-from matplotlib.pyplot import MultipleLocator
 
 LOG = logging.getLogger(__name__)
 
-__version__ = "0.1.0"
-__author__ = ("Liu huifang",)
-__email__ = "liuhuifang@grandomics.com"
+__version__ = "1.1.0"
+__author__ = ("Xingguo Zhang",)
+__email__ = "invicoun@foxmail.com"
 __all__ = []
 
 
-def get_color(species):
+def read_tsv(file, sep=None):
 
-    sp_cl = []
-    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#1a55FF']
-    num_cl = sorted(list(set(species)))
-    num_co = colors[0:len(num_cl)]
+    LOG.info("Reading message from %r" % file)
 
-    for i in species:
-        for j in range(0,len(num_cl)):
-            if i == num_cl[j]:
-                sp_cl.append(colors[j])
-    return sp_cl, num_cl, num_co
+    for line in open(file):
+        line = line.strip()
+
+        if not line or line.startswith("#"):
+            continue
+
+        yield line.split(sep)
 
 
-def plot_species(args):
+def read_species(file):
 
-    sp = []
-    group = []
-    number = []
-    f_out = open('%s.top10_species.tsv' % args.name,'w')
+    print("#Species\tReads Number\tTaxonomy")
 
-    f_out.write('#Species\tReads Number\tTaxonomy\n')
-    with open(args.input, 'r') as f_open:
-        for line in f_open:
-            tem = line.strip().split('\t')
-            if tem[1] == 'Unmap':
-                f_out.write('%s\t%s\t%s\n' % (tem[1], tem[2], tem[0]))
-            else:
-                group.append(tem[0])
-                sp.append(tem[1])
-                number.append(float(tem[2]))
+    n = 0
+    r = {}
+    species = []
+    total = 0
+    for line in read_tsv(file, "\t"):
+        total += float(line[2])
 
-    sp_top10 = []
-    group_top10 = []
-    number_top10 = []
-    percent_top10 = []
-    for i in range(0,10):
-        sp_top10.append(sp[i])
-        group_top10.append(group[i])
-        number_top10.append(number[i])
-        per = round(100 * float(number[i])/sum(number),2)
-        per = str(per) + '%'
-        percent_top10.append(per)
-        f_out.write('%s\t%s\t%s\n' % (sp[i], int(number[i]), group[i]))
-    f_out.close()
+        if n >=10:
+            continue
+        print("%s\t%s\t%s" % (line[1], line[2], line[0]))
+        if line[1] == 'Unmap':
+            continue
+        n += 1
+        if line[0] not in r:
+            r[line[0]] = [[], []]
+        species.append(line[1])
+        r[line[0]][0].append(n)
+        r[line[0]][1].append(float(line[2]))
 
-    max_value = max(number_top10)/30
-    plt.switch_backend('agg')
+    if "Bacteria" in r:
+        print("#!基于污染分析分析结果显示，数据存在明显细菌的污染对后续分析可能存在一定的影响")
+    else:
+        print("#!基于污染分析分析结果显示，数据无明显细菌的污染可以进行后续分析")
+
+    return r, species, total
+
+
+def plot_species(r, species, total, prefix="out"):
+
+    COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',
+              '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#1a55FF']
+
     fig, ax = plt.subplots()
-    category_colors, x_legend, x_colors = get_color(group_top10)
+    n = 0
+    maxy = 0
+    rects = []
+    for i in r:
+        rects += ax.bar(r[i][0], r[i][1], color=COLORS[n], label=i)
+        #rects.append(rects)
+        n += 1
+        if max(r[i][1]) >= maxy:
+            maxy = max(r[i][1])
 
-    xs1 = range(len(sp_top10))
-    ax.bar(xs1, number_top10, color = category_colors)
-    sp_top10.insert(0, 'a')
-    plt.xticks(xs1, sp_top10)
-
-    x_major_locator = MultipleLocator(1)
-    ax.xaxis.set_major_locator(x_major_locator)
-    patches = [mpatches.Patch(color=x_colors[i], label=x_legend[i]) for i in range(len(x_colors))]
-    ax.legend(handles=patches)
+    plt.xticks(range(1, len(species)+1), species)
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right")
-    ax.set_ylim(0, max(number_top10) + max(number_top10)/10)
+    ax.set_ylim(0, maxy+maxy/10)
 
-    for i in xs1:
-        ax.text(xs1[i], number_top10[i] + max_value, percent_top10[i], horizontalalignment='center', verticalalignment='center')
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{:.2f}%'.format(height*100.0/total),
+                    xy=(rect.get_x() + rect.get_width()/2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+    plt.legend(loc='upper right',
+               frameon=False,
+               fontsize="large",
+               labelspacing=0.3,
+               handlelength=0.6,
+               handleheight=0.6,
+               handletextpad=0.5)
+
     fig.tight_layout()
+    plt.savefig('%s.top10_species.pdf' % prefix)
+    plt.savefig('%s.top10_species.png' % prefix, dpi=700)
 
-    plt.savefig('%s.top10_species.pdf' % args.name)
-    plt.savefig('%s.top10_species.png' % args.name, dpi=700)
+
+def add_hlep_args(parser):
+
+    parser.add_argument('input', metavar='FILE', type=str,
+        help='Input species statistics file(stat_species.tsv)')
+    parser.add_argument('-p', '--prefix', metavar='FILE', type=str, default="out",
+        help='Output file prefix, default=out.')
+
+    return parser
 
 
 def main():
+
     logging.basicConfig(
         stream=sys.stderr,
         level=logging.INFO,
         format="[%(levelname)s] %(message)s"
     )
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='''
-        
-For exmple:
-        python plot_stat_species -i txt.stat_species.tsv
-        
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='''
+attention:
+    plot_stat_species.py stat_species.tsv -p name >name.top10_species.tsv
 version: %s
 contact:  %s <%s>\
-    ''' % (__version__, " ".join(__author__), __email__))
+        ''' % (__version__, ' '.join(__author__), __email__))
 
-    parser.add_argument('-i', '--input',required=True,
-                        help='a file,such as txt.stat_species.tsv')
-    parser.add_argument('-n','--name', metavar='STR', default='out',
-                        help='Output file name')
+    args = add_hlep_args(parser).parse_args()
+    r, species, total = read_species(args.input)
 
-    args = parser.parse_args()
-
-    plot_species(args)
-
+    plot_species(r, species, total, args.prefix)
+    
 
 if __name__ == "__main__":
 
