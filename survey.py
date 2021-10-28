43e6501259c434622c6d50c54c3fa3204fd6cfc8
sy520 <36355222+zxgsy520@users.noreply.github.com>
2021-10-28 11:57:18 +0800

    Add files via upload

--- survey.py
+++ survey.py
@@ -5,7 +5,7 @@ import argparse
 
 from survey.parser import *
 from survey.ngs_qc import ngs_qc
-from survey.filter_contamination import filter_contamination
+from survey.filter_cont import filter_cont
 from survey.kmer_denovo import kmer_denovo
 from survey.all import run_all
 from survey import __version__, __email__, __author__
@@ -25,9 +25,9 @@ def add_survey_parser(parser):
     ngs_qc_parser = add_ngs_qc_args(ngs_qc_parser)
     ngs_qc_parser.set_defaults(func=ngs_qc)
 
-    filter_contamination_parser = subparsers.add_parser('filter_contamination', help="Sequence contamination filtering")
-    filter_contamination_parser = add_filter_contamination_args(filter_contamination_parser)
-    filter_contamination_parser.set_defaults(func=filter_contamination)
+    filter_cont_parser = subparsers.add_parser('filter_cont', help="Sequence contamination filtering")
+    filter_cont_parser = add_filter_cont_args(filter_cont_parser)
+    filter_cont_parser.set_defaults(func=filter_cont)
 
 
     kmer_denovo_parser = subparsers.add_parser('kmer_denovo', help="Genomic k-mer estimation and assembly")
@@ -42,10 +42,11 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="""
-Nexomics Eukaryotic Genome Analysis Pipeline
+URL: https://github.com/zxgsy520/survey
+Survey: Eukaryotic survey analysis Pipeline
 
 version: %s
-contact:  %s <%s>\
+contact: %s <%s>\
         """ % (__version__, " ".join(__author__), __email__))
 
     parser = add_survey_parser(parser)
